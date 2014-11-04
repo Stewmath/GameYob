5629ec606bd6e359c48b49c9642c62ef3996ab65
Matthew Stewart <stewartmatthew6@gmail.com>
2014-11-03 20:43:22 -0500

    Clean up test code, extra permissions for cia

--- platform/3ds/main.cpp
+++ platform/3ds/main.cpp
@@ -12,41 +12,18 @@
 #include "gbmanager.h"
 #include "printconsole.h"
 
-u32 srv_handle;
-
-Result registerService(char* name) {
-    u32* cmdBuf = getThreadCommandBuffer();
-
-    cmdBuf[0] = 0x30100;
-    strcpy((char*)&cmdBuf[1], name);
-    cmdBuf[3] = strlen(name);
-    cmdBuf[4] = 0;
-
-    svcSendSyncRequest(srv_handle);
-
-    return cmdBuf[1];
-}
-
 int main(int argc, char* argv[])
 {
-	srvInit();	
+    srvInit();
+
 	aptInit();
 	hidInit(NULL);
 	gfxInit();
     fsInit();
 
-    /*
-    // Delay before aptSetupEventHandler
-    for (int i=0; i<10; i++)
-        system_waitForVBlank();
-
-	aptSetupEventHandler();
-    */
-
     consoleInitBottom();
 
     fs_init();
-
     mgr_init();
 
 	initInput();
@@ -54,20 +31,6 @@ int main(int argc, char* argv[])
     readConfigFile();
 
     printf("GameYob 3DS\n\n");
-
-    printAndWait("Haxors YAY\n");
-
-    Handle irHandle;
-    Result res = svcConnectToPort(&srv_handle, "srv:pm");
-
-    printAndWait("srv:pm Result %.8x\n", res);
-
-    res = registerService("ir:u");
-    printAndWait("Register result: %.8x\n", res);
-
-    res = srvGetServiceHandle(&irHandle, "ir:u");
-
-    printAndWait("ir:u Result %.8x\n", res);
 
     mgr_selectRom();
 
